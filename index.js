d3a368b554c049ea92a2671136ea72e689a281fc
DEAMBROGGI <adrian.m.deambroggi@gmail.com>
2023-04-05 21:51:53 -0300

    paginas dinamicas

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 
-var datosEventos //Contener el array con todos los eventos
-var fechaBase //Es la fecha establecida en el archivo JSON
 var texto
+let eventos = datosAmazing.eventos
+let fechaBase = datosAmazing.fechaActual
+let textoHTML = document.getElementById("form")
+let ulNombreEventos = document.getElementById("eventos")
 
 function ChangeTemplateLayaout() {
 
     switch (initialState.paginaANavegar) {
 
         case "EventosPasados":
-            texto = "Estas en la pagina de Eventos Pasados"
+            let eventosPasados = eventos.filter(evento =>  evento.date < fechaBase)
+            pintarHTML(eventosPasados)
             //console.log("Ocultar Contactos, estadisticas y Filtrar datosEventos donde los eventos sean menores a la fechaBase")
             break;
         case "EventosFuturos":
-            texto = "Estas en la pagina de Eventos Futuros"
+            let eventosFuturos = eventos.filter(evento =>  evento.date > fechaBase)
+            pintarHTML(eventosFuturos)
             //console.log("Ocultar Contactos, estadisticas y Filtrar datosEventos donde los eventos sean mayores a la fechaBase")
             break;
         case "Contactos":
-            texto = "Estas en la pagina de Contactos"
+             
+            textoHTML.innerHTML = 
+            `
+            <form action="">
+            <div class="form_input">
+            <label for="email"><i class="fa-solid fa-user"></i></label>
+            <input type="email" name="email" placeholder="email@email.com">
+            </div>
+            <div class="form_input">
+            <label for="pass"><i class="fa-solid fa-lock"></i></label>
+            <input type="password" name="password" placeholder="Password">
+            </div>
+            </form>
+            `
+            ulNombreEventos.innerHTML =""
             //console.log("Ocultar las Cards o Estadisticas y va a mostrar el formulario de contactos")
             break;
         case "Estadisticas":
             texto = "Estas en la pagina de Estadistica"
+            textoHTML.innerHTML = texto
+            ulNombreEventos.innerHTML = ""
             //console.log("Ocultar las Cards o Contactos y va a mostrar Tabla de estadisticas")
             break;
         default:
@@ -29,11 +49,10 @@ function ChangeTemplateLayaout() {
             InitAppStyle.style.backgroundColor = "pink"
             InitAppStyle.disabled = true
 
-            texto = "Estas en la pagina HOME"
+            pintarHTML(eventos)
         //console.log("Ocultar Contactos, estadisticas y Mostrar toda la info de datosEventos = todos los eventos")
     }
 
-    document.getElementById("Pagina").innerHTML = texto
 }
 
 ChangeTemplateLayaout()
@@ -60,3 +79,22 @@ for (let index = 0; index < buttons.length; index++) {
 }   //comentario de test
 
 
+function pintarHTML(array){
+    
+    let nombreEventosHTML = ""
+    array.map(evento =>
+        nombreEventosHTML +=
+        `
+        <div class="item">
+        <img src="./Imagenes/${evento.image}" alt="${evento.name}">
+        <p class="titulo_dos">${evento.name}</p>
+        <div class="detalles">
+          <p class="precio">Precio: $${evento.price}</p>
+          <p class="boton_d"><a href="./Pages/Detalles.html?id=${evento.id}">Detalles</a></p>
+        </div>
+      </div>
+        `
+    )
+    ulNombreEventos.innerHTML = nombreEventosHTML
+    textoHTML.innerHTML = ""
+}
