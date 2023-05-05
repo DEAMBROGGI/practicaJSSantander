ce0478c8cc7c759c905f4d4bdb10effe62ca5000
DEAMBROGGI <adrian.m.deambroggi@gmail.com>
2023-05-04 21:52:36 -0300

    stats por categorias ordenadas por ingresos

--- JS/stast.js
+++ JS/stast.js
@@ -1,4 +1,4 @@
-function initStats() {
+async function initStats() {
     console.log(eventos)
 
     // Obtenemos las categorias unicas
@@ -64,8 +64,8 @@ function initStats() {
                 categoria.totalAsistenciaEstimada = totalAsistenciaEstimada
             }
         }
-        categoria.porcentajeDeAsistencia = "%" + (totalAsistencia * 100) / totalCapacidadPasados
-        categoria.porcentajeDeEstimacion = "%" + (totalAsistenciaEstimada * 100) / totalCapacidadFuturos
+        categoria.porcentajeDeAsistencia = "%" + ((totalAsistencia * 100) / totalCapacidadPasados).toFixed(2)
+        categoria.porcentajeDeEstimacion = "%" + ((totalAsistenciaEstimada * 100) / totalCapacidadFuturos).toFixed(2)
 
         //Calculo el total de ingresos (PASADOS)
         let totalIngreso = 0
@@ -80,6 +80,96 @@ function initStats() {
     })
 
     console.log(ingresoYassitencia)
+    //Separo los eventos pasados que son los que poseen asistencia
+    let eventosPasados = []
+    let eventosFuturos = []
+    await eventos.filter(evento => {
+        if (evento.assistance) {
+            eventosPasados.push(evento)
+        } else { eventosFuturos.push(evento) }
+    })
+    //le agregop a cada evento una propiedad calculando el porcentaje de asistencia en funcion de la capacidad del evento
+
+    eventosPasados.map(evento => {
+        evento.porcentajeAsistencia = evento.assistance * 100 / evento.capacity
+    })
+
+    let asistenciaEventos = []
+    eventosPasados.filter(evento => { asistenciaEventos.push(evento.porcentajeAsistencia) })
+    // //Busco el mayor valor y filtro por este el array de eventos
+    let mayor = Math.max(...asistenciaEventos)
+    let eventoMayorAsistencia = eventos.filter(evento => evento.porcentajeAsistencia === mayor)
+
+    // Busco el menor valor y filtro por este el array de eventos pasados
+    let menor = Math.min(...asistenciaEventos)
+    let eventoMenorAsistencia = eventos.filter(evento => evento.porcentajeAsistencia === menor)
+
+    // Busco el evento con mayor capacidad por ordenamiento descendente
+    let mayorCapacidad = eventos.sort((a, b) => { return b.capacity - a.capacity })
+
+
+    //GENERO DATOS EN TABLA DE EVENTOS CON MAYOR Y MENOR ASISTENCIA Y CAPACIDAD
+    var rowMayoresYmenores = document.getElementById("mayoresymenores")
+    rowMayoresYmenores.innerHTML = ""
+    var tdMayorAsistencia = document.createElement("td")
+    var tdMenorAsistencia = document.createElement("td")
+    var tdMayorCapacidad = document.createElement("td")
+
+    rowMayoresYmenores.append(tdMayorAsistencia)
+    tdMayorAsistencia.append(eventoMayorAsistencia[0].name + " %" + eventoMayorAsistencia[0].porcentajeAsistencia.toFixed(2))
+
+    rowMayoresYmenores.append(tdMenorAsistencia)
+    tdMenorAsistencia.append(eventoMenorAsistencia[0].name + " %" + eventoMenorAsistencia[0].porcentajeAsistencia.toFixed(2))
+
+    rowMayoresYmenores.append(tdMayorCapacidad)
+    tdMayorCapacidad.append(mayorCapacidad[0].name + "(" + mayorCapacidad[0].category + ")")
+
+    //GENERAR LOS DATOS DE CATEGIRIAS DE EVENTOS FUTUROS
+
+    var tablaFuturos = document.getElementById("statsFuturos")
+
+    ordenarFuturos = []
+    ordenarFuturos.push(...ingresoYassitencia.sort((a, b) => {
+        return b.estimacionIngresos - a.estimacionIngresos
+    }))
+
+    ordenarFuturos.map(evento => {
+        if (evento.estimacionIngresos > 0) {
+            tablaFuturos.innerHTML +=
+                `
+        <tr>
+              <td>${evento.categoria}</td>
+              <td>$${evento.estimacionIngresos}</td>
+              <td>${evento.porcentajeDeEstimacion}</td>
+            </tr>       
+        `
+        }
+    })
+
+
+    //GENERAR LOS DATOS DE CATEGIRIAS DE EVENTOS Pasados
+
+    var tablaPasados = document.getElementById("statsPasados")
+
+    let ordenarPasados = []
+    ordenarPasados.push(...ingresoYassitencia.sort((a, b) => {
+        return b.ingresos - a.ingresos
+    }))
+
+    ordenarPasados.map(evento => {
+        if (evento.ingresos > 0) {
+            tablaPasados.innerHTML +=
+                `
+     <tr>
+           <td>${evento.categoria}</td>
+           <td>$${evento.ingresos}</td>
+           <td>${evento.porcentajeDeAsistencia}</td>
+         </tr>       
+     `
+        }
+    })
 
 }
+
+
 
